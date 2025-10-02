5eface26e19b82c000cab6bf5d46e2372635b72a
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2025-10-02 14:46:56 +0200

    Add some using key mapping to run lua code directly

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,12 +6,17 @@ local package_cpath_str = os.getenv("HOME") .. "/.luarocks/lib/lua/5.1/?.so;"
 package.path = package.path .. ";" .. package_path_str
 package.cpath = package.cpath .. ";" .. package_cpath_str
 
+
 -- Disable vim.lsp default keymaps. Using Coc.nvim instead.
-vim.keymap.del('n', 'grr', { silent = true })
-vim.keymap.del('n', 'grn', { silent = true })
-vim.keymap.del('n', 'gri', { silent = true })
-vim.keymap.del('n', 'gra', { silent = true })
-vim.keymap.del('n', 'grt', { silent = true })
+if vim.g.default_lsp_keymaps_del == nil then
+  vim.keymap.del('n', 'grr', { silent = true })
+  vim.keymap.del('n', 'grn', { silent = true })
+  vim.keymap.del('n', 'gri', { silent = true })
+  vim.keymap.del('n', 'gra', { silent = true })
+  vim.keymap.del('n', 'grt', { silent = true })
+
+  vim.g.default_lsp_keymaps_del = true
+end
 
 require("config.options")
 require("config.lazy")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -14,15 +14,6 @@ vim.api.nvim_command('syntax on')
 -- Leader key mapping
 vim.g.mapleader = ','
 
-function Map(mode, lhs, rhs, opts)
-  local options = { noremap = true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
 -- Disable python2 provider
 vim.g.loaded_python_provider = 0
 
@@ -34,10 +25,16 @@ vim.g.loaded_ruby_provider = 0
 
 
 -- Opens vimrc file
---[[
-   [Map('n', '<leader>ev', ':vsplit $MYVIMRC<CR>')
-   [
-   [-- Source vimrc file
-   [Map('n', '<leader>es', ':source $MYVIMRC<CR>')
-   ]]
+vim.keymap.set('n', '<leader>ev', ':vsplit $MYVIMRC<CR>')
+
+local options_file_path = vim.fn.fnamemodify(vim.fn.expand('$MYVIMRC'), ':h') .. '/lua/config/options.lua'
+vim.keymap.set('n', '<leader>eo', ':vsplit ' .. options_file_path .. '<CR>')
+
+-- Source vimrc file
+vim.keymap.set('n', '<leader>es', ':source $MYVIMRC<CR>')
+
+--- Run lua code key mapping
+vim.keymap.set('n', '<leader>el', ':luafile %<CR>')
+vim.keymap.set('n', '<leader>ee', ':.lua<CR>')
+vim.keymap.set('v', '<leader>ee', ':lua<CR>')
 
